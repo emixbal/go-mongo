4e13faf0522edc10c14672bac31f57eb325e003b
iqbal <emixbal@gmail.com>
2022-12-16 15:39:49 +0700

    response interface

---
+++ app/routers/jamaah.routers.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	jamaahcontroller "go-mongo/app/controllers/jamaahController"
+	"go-mongo/app/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Jamaah(router fiber.Router) {
+	r := router.Group("/jamaah")
+
+	r.Get("/", middlewares.ExampleMiddleware, jamaahcontroller.JamaahList)
+	r.Post("/", middlewares.ExampleMiddleware, jamaahcontroller.JamaahList)
+
+}
